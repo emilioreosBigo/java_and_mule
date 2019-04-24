801d3dcb273bd239c2c00cead3777d9aa0b8e571
Emilio Reos <emilio.reos@bigo.mx>
2019-04-24 17:37:11 -0500

    No redownload mule

--- lib/mule.sh
+++ lib/mule.sh
@@ -7,11 +7,18 @@ install_mule() {
   echo -n "-----> Installing Mule..."
   let start=$(nowms)
   MULE_COMMON_BUILDPACK=${MULE_COMMON_BUILDPACK:-https://repository-master.mulesoft.org/nexus/service/local/repositories/releases/content/org/mule/distributions/mule-standalone/3.9.0/mule-standalone-3.9.0.tar.gz}
-  mkdir -p $HOME/mule-standalone-3.9.0/
-  curl --retry 3 --silent --location $MULE_COMMON_BUILDPACK | tar xzm -C $HOME/mule-standalone-3.9.0/ --strip-components=1
-  chmod +x $HOME/mule-standalone-3.9.0/bin/mule
-  chmod +w $HOME/mule-standalone-3.9.0/apps/
-  chmod +w $HOME/mule-standalone-3.9.0/
+  
+  if [ ! -f $HOME/mule-standalone-3.9.0 ] ; then
+
+    echo -n " Downloading .... "
+
+    mkdir -p $HOME/mule-standalone-3.9.0/
+    curl --retry 3 --silent --location $MULE_COMMON_BUILDPACK | tar xzm -C $HOME/mule-standalone-3.9.0/ --strip-components=1
+    chmod +x $HOME/mule-standalone-3.9.0/bin/mule
+    chmod +w $HOME/mule-standalone-3.9.0/apps/
+    chmod +w $HOME/mule-standalone-3.9.0/
+
+  fi
 
   export MULE_HOME=$HOME/mule-standalone-3.9.0/
 
