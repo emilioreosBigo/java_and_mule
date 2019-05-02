b6b3c4208c506500dfec6492134c2755aac9326a
Emilio <emilio.reos@bigo.mx>
2019-05-02 16:48:24 -0500

    Test

--- lib/mule.sh
+++ lib/mule.sh
@@ -4,23 +4,21 @@
 install_mule() {
   local install_dir=${1}
   local cache_dir=${2}
-  echo -n "-----> Installing Mule...$install_dir"
+  echo -n "-----> Installing Mule...$cache_dir"
   let start=$(nowms)
   MULE_COMMON_BUILDPACK=${MULE_COMMON_BUILDPACK:-https://repository-master.mulesoft.org/nexus/service/local/repositories/releases/content/org/mule/distributions/mule-standalone/3.9.0/mule-standalone-3.9.0.tar.gz}
   
-  if [ ! -d dist/mule-standalone-3.9.0 ] ; then
+  if [ ! -d $cache_dir/mule-standalone-3.9.0 ] ; then
 
     echo -n " Downloading .... "
 
-    mkdir -p dist/mule-standalone-3.9.0/
-    curl --retry 3 --silent --location $MULE_COMMON_BUILDPACK | tar xzm -C dist/mule-standalone-3.9.0/ --strip-components=1
-    chmod +x dist/mule-standalone-3.9.0/bin/mule
-    chmod +w dist/mule-standalone-3.9.0/apps/
-    chmod +w dist/mule-standalone-3.9.0/
+    mkdir -p $cache_dir/mule-standalone-3.9.0/
+    curl --retry 3 --silent --location $MULE_COMMON_BUILDPACK | tar xzm -C $cache_dir/mule-standalone-3.9.0/ --strip-components=1
+    chmod +x $cache_dir/mule-standalone-3.9.0/bin/mule
+    chmod +w $cache_dir/mule-standalone-3.9.0/apps/
+    chmod +w $cache_dir/mule-standalone-3.9.0/
 
   fi
-
-  echo -n "$(ls -lha dist)"
 
   #export MULE_HOME=/app/mule-standalone-3.9.0/
 
